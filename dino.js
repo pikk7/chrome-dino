b91f20146c8acec2cfbe2d5743cdfb3ceb30f97a
pikk7 <durza13@gmail.com>
2023-07-19 12:29:38 +0200

    Dino logic

---
+++ dino.js
@@ -0,0 +1,70 @@
+import {
+  getCustomProperty,
+  incrementCustomProperty,
+  setCustomProperty,
+} from "./updateCustomProperty.js";
+
+const dinoElement = document.querySelector(".dino");
+const JUMP_SPEED = 0.45;
+const GRAVITY = 0.011;
+const DINO_FRAME_COUNT = 2; //two image for animation
+const FRAME_TIME = 100;
+
+let isJumping;
+let dinoFrame;
+let currentFrameTime;
+let yVelocity;
+
+export function setupDino() {
+  isJumping = false;
+  dinoFrame = 0;
+  currentFrameTime = 0;
+  yVelocity = 0;
+  setCustomProperty(dinoElement, "--bottom", 0);
+  document.removeEventListener("keydown", onJump);
+
+  document.addEventListener("keydown", onJump);
+}
+
+export function updateDino(delta, speedScale) {
+  handleRun(delta, speedScale);
+  handleJump(delta);
+}
+
+function handleRun(delta, speedScale) {
+  if (isJumping) {
+    dinoElement.src = `imgs/dino-stationary.png`;
+    return;
+  }
+
+  if (currentFrameTime >= FRAME_TIME) {
+    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT;
+    dinoElement.src = `imgs/dino-run-${dinoFrame}.png`;
+    currentFrameTime -= FRAME_TIME;
+  }
+
+  currentFrameTime += delta * speedScale;
+}
+
+function handleJump(delta) {
+  if (!isJumping) {
+    return;
+  }
+
+  incrementCustomProperty(dinoElement, "--bottom", yVelocity * delta);
+
+  if (getCustomProperty(dinoElement, "--bottom") <= 0) {
+    setCustomProperty(dinoElement, "--bottom", 0);
+    isJumping = false;
+  }
+
+  yVelocity -= GRAVITY * delta;
+}
+
+function onJump(e) {
+  if (e.code !== "Space" || isJumping) {
+    return;
+  }
+  yVelocity = JUMP_SPEED;
+  isJumping = true;
+}
